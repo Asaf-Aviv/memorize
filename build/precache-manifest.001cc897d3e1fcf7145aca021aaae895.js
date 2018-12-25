e632eed4187c7599c204c59df10f8c872a7e6c5f
unknown <asafaviv89@gmail.com>
2018-12-25 14:55:40 +0200

    build

---
+++ build/precache-manifest.001cc897d3e1fcf7145aca021aaae895.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f7f637fa69601b1d95bcc06f376a1d9",
+    "url": "/static/media/cherries.6f7f637f.svg"
+  },
+  {
+    "revision": "435954930d6ee2f9813d",
+    "url": "/static/css/main.a77279d9.chunk.css"
+  },
+  {
+    "revision": "49426452059e4450eeb0",
+    "url": "/static/js/1.49426452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6952c106ebef976a4ac9389ec34d9c2f",
+    "url": "/static/media/menu.6952c106.wav"
+  },
+  {
+    "revision": "483fb361ea871999f0277e74fa066dea",
+    "url": "/static/media/back.483fb361.wav"
+  },
+  {
+    "revision": "9a5e6ba6120de3e62604b617a6f3a860",
+    "url": "/static/media/match.9a5e6ba6.wav"
+  },
+  {
+    "revision": "de126af11fb44fe431325efdc90fde04",
+    "url": "/static/media/menu.de126af1.svg"
+  },
+  {
+    "revision": "96037b82cd18cfb7b4ce188d97d8a771",
+    "url": "/static/media/apple.96037b82.svg"
+  },
+  {
+    "revision": "892b8f16d7a1816e344383beb520ad73",
+    "url": "/static/media/banana.892b8f16.svg"
+  },
+  {
+    "revision": "435954930d6ee2f9813d",
+    "url": "/static/js/main.43595493.chunk.js"
+  },
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "812f3c7a3b421de4175dda432498a564",
+    "url": "/static/media/lemon.812f3c7a.svg"
+  },
+  {
+    "revision": "0c89232ea4292575a15e06364f1e9824",
+    "url": "/static/media/pear.0c89232e.svg"
+  },
+  {
+    "revision": "c9276cc2012607adb28a93620181325c",
+    "url": "/static/media/pineapple.c9276cc2.svg"
+  },
+  {
+    "revision": "509d71bb6b6d748d1f2d4f84b8f9d1ef",
+    "url": "/static/media/raspberry.509d71bb.svg"
+  },
+  {
+    "revision": "ba199f9c284e33d57667878570962b66",
+    "url": "/static/media/tomato.ba199f9c.svg"
+  },
+  {
+    "revision": "4bb012a37bd18935e1e1274a98d54ebc",
+    "url": "/static/media/watermelon.4bb012a3.svg"
+  },
+  {
+    "revision": "c6842476bd9046768fd18db70510b2f9",
+    "url": "/static/media/brushed-alum.c6842476.png"
+  },
+  {
+    "revision": "4697588bf4e7afc775c0f1fd1a85d810",
+    "url": "/index.html"
+  }
+];
